6200b6886628dc416c162816ab2975b284cc17fb
nr112358 <nr112358@users.noreply.github.com>
2015-02-27 18:51:16 -0600

    new function added header updated with correct student info

--- EE4377.m
+++ EE4377.m
@@ -1,7 +1,7 @@
-% EExxxx Project 1
+% EE4377 Project 1
 % MATLAB code for xxxxxxxxxxxxx
-% Your Name and Last Name
-% Date
+% Nicki Rios
+% Feb. 27, 2015
 
 clc      % Clear screen 
 clear    % Clear all variables
@@ -9,10 +9,13 @@ A = 3; f= 1000; omega = 2*pi*f;
 angle = degtorad(33); T=1/f;
 t=linspace(-3*T,3*T,1000);
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+f(t)=cos(omega*t)*sin(omega*t)*u(t-1);
+subplot(4,1,4)
+plot(t,f(t), 'linewidth',2);grid on
